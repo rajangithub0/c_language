f778f73fb3f28557223ceeb3effd2cdb9bc0522e
Rajan <rajan.rana6@gmail.com>
2024-05-05 21:21:45 +0530

    basic of c language

---
+++ datatype.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+// this code define the datatypes wrok in c language.
+int main(){
+    // int age=23;
+    // float PI=3.14;
+    // char hashtag='#';
+    printf("hello c language\n");
+    printf("hello c language\n");
+    printf("hello c language\n");
+    printf("hello c language\n");
+    printf("hello c language\n");
+    printf("welcome to c language\n");
+    return 0;
+
+    /*this is a multiple line comment*/
+}
